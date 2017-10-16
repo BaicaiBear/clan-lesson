c4769068637bf7e3098e657e60a5ab1040064ef0
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-10-16 19:49:55 +0800

    Create 3.cpp

---
+++ 2nd/3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//#include <windows.h>
+
+int main()
+{ 
+    printf("笑脸:\001\001\001\001\001\001\001\n");
+    printf("红黑桃:\003\006\n");
+    printf("听个响:\a\a\a\a\a\a\a\a\a\n");
+    printf("再看字母:\101\102\n");
+    getchar();
+
+    return 0;
+}
