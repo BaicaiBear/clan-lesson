f9e7f5b70701aec2233534c3163dbc9749778398
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-10-30 19:19:28 +0800

    Update 3.cpp

--- 4th/3.cpp
+++ 4th/3.cpp
@@ -4,13 +4,18 @@
 int main()
 {
     int a,b;
+    //给两个变量赋值 
     scanf("%d%d",&a,&b);
+    
+    //判断 
     if(a>b)
-           printf("%d\n",b);
+           printf("%d\n",b);//当a>b时，输出b 
     else if(a<b)
-         printf("%d\n",a);
+         printf("%d\n",a);//当a<b时，输出a 
     else
-        printf("%d\n",a);
+        printf("%d\n",a);//当a=b时，输出a
+    
+    //暂停运行 
     system("pause");
     return 0;
 }
