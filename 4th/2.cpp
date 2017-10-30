07a6b8a87520a1f1944fa62f8e3caa78cf241586
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-10-30 19:19:06 +0800

    Update 2.cpp

--- 4th/2.cpp
+++ 4th/2.cpp
@@ -4,11 +4,16 @@
 int main()
 {
     char c;
+    //给变量赋值
     c = getchar();
+    
+    //判断
     if(c=='y'||c=='Y')
                       printf("yes");
     else
         printf("no");
+        
+    //暂停运行
     system("pause");
     return 0;
 }
