df5f27e4db6ff62c185ca3581da50f54d8fa6c49
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-10-30 18:46:23 +0800

    Update 1.cpp

--- 4th/1.cpp
+++ 4th/1.cpp
@@ -4,12 +4,22 @@
 int main()
 {
     char c1,c2,a;
+    
+    //给两个字符型变量赋值
     c1 = getchar();
     c2 = getchar();
+    //输出未交换的两个变量
+    printf("c1='%c' c2='%c'\n",c1,c2);
+    
+    //交换过程
     a = c1;
     c1 = c2;
     c2 = a;
-    printf("%c%c\n",c1,c2);
+    
+    //输出交换后的两个变量
+    printf("c1='%c' c2='%c'\n",c1,c2);
+    
+    //暂停运行
     system("pause");
     return 0;
 }               
