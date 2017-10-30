412d3bc7589feaceeb3951b81d43eafea1c33e39
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-10-30 19:03:47 +0800

    Create 3.cpp

---
+++ 4th/3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <windows.h> 
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+           printf("%d\n",b);
+    else if(a<b)
+         printf("%d\n",a);
+    else
+        printf("%d\n",a);
+    system("pause");
+    return 0;
+}
+    
