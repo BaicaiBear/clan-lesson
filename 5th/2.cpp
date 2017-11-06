a6c6f93e2e67aff0827b5c894fd5af337ad98c5c
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-11-06 19:16:26 +0800

    Create 2.cpp

---
+++ 5th/2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b) printf("%d",a);
+    else printf("%d",b);
+    system("pause");
+    return 0;
+}
