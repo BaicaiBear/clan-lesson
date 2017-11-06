96cbd6eaad0afc1e5927ebf015182be341420c21
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-11-06 19:23:04 +0800

    Create 4.cpp

---
+++ 5th/4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2==0) printf("偶数\n");
+    else printf("奇数\n");
+    system("pause");
+    return 0;
+} 
