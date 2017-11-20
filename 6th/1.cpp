f55366352404cf8e518ff818a94dbd790668dde8
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-11-20 19:30:10 +0800

    Create 1.cpp

---
+++ 6th/1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+int main()
+{
+  float a,b,c,t;
+  scanf("%f,%f,%f", &a,&b,&c);
+  if(a>b){t=a;a=b;b=t;}
+  if(a>c){t=a;a=c;c=t;}
+  if(b>c){t=b;b=c;c=t;}
+  printf("%.2f,%.2f,%.2f",a,b,c);
+  system("PAUSE");
+  return 0;
+}
