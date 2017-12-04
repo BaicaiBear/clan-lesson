d65fdfab4396c2d69658c9c2b3b96cd9fb1f8244
BaicaiBear <xiaoxiongbaicai@outlook.com>
2017-12-04 19:10:58 +0800

    Update 3.cpp

--- 8th/3.cpp
+++ 8th/3.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
-#include <windows.h>
+
+//求1000以内偶数的和
+
+/**
+样例输入   无     无
+样例输出 250500 250500
+**/
 
 int main(){
     int i=0,sum=0;
@@ -9,6 +15,5 @@ int main(){
      }
     while(i<=1000);
     printf("%d",sum);
-    system("PAUSE");
     return 0;
 } 
